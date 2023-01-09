433567b04fb991caed7a4cb6566c31bd8ad0dc26
Toshinori Sugita <s.toshi0607@gmail.com>
2023-01-09 17:39:27 +0900

    add create version

--- internal/dialogflow/dialogflow.go
+++ internal/dialogflow/dialogflow.go
@@ -21,16 +21,19 @@ type Config struct {
 	TargetEnvID     string
 }
 
-const asiaNorthEast1Endpoint = "asia-northeast1-dialogflow.googleapis.com"
+const (
+	asiaNorthEast1Endpoint = "asia-northeast1-dialogflow.googleapis.com"
+	defaultFlowID          = "00000000-0000-0000-0000-000000000000"
+)
 
 func Deploy(ctx context.Context, cfg Config, version string) error {
 	if cfg.BaseAgentID != cfg.TargetAgentID {
-		c, err := cx.NewAgentsClient(ctx, option.WithEndpoint(fmt.Sprintf("%s:443", asiaNorthEast1Endpoint)))
+		ac, err := cx.NewAgentsClient(ctx, option.WithEndpoint(fmt.Sprintf("%s:443", asiaNorthEast1Endpoint)))
 		if err != nil {
 			return failure.Wrap(err)
 		}
 		defer func() {
-			if err := c.Close(); err != nil {
+			if err := ac.Close(); err != nil {
 				logger.Logger.Error("failed to close agent client", err)
 			}
 		}()
@@ -39,7 +42,7 @@ func Deploy(ctx context.Context, cfg Config, version string) error {
 			Name:        baseAgent(cfg),
 			Environment: baseEnvironment(cfg),
 		}
-		exportOp, err := c.ExportAgent(ctx, exportReq)
+		exportOp, err := ac.ExportAgent(ctx, exportReq)
 		if err != nil {
 			return failure.Wrap(fmt.Errorf("failed to export agent, error: %v", err))
 		}
@@ -55,7 +58,7 @@ func Deploy(ctx context.Context, cfg Config, version string) error {
 			Name:  targetAgent(cfg),
 			Agent: &cxpb.RestoreAgentRequest_AgentContent{AgentContent: content},
 		}
-		restoreOp, err := c.RestoreAgent(ctx, restoreReq)
+		restoreOp, err := ac.RestoreAgent(ctx, restoreReq)
 		if err != nil {
 			return failure.Wrap(fmt.Errorf("failed to restore the agent, error: %v", err))
 		}
@@ -66,7 +69,32 @@ func Deploy(ctx context.Context, cfg Config, version string) error {
 		}
 	}
 
-	// publish version for target
+	vc, err := cx.NewVersionsClient(ctx, option.WithEndpoint(fmt.Sprintf("%s:443", asiaNorthEast1Endpoint)))
+	if err != nil {
+		return failure.Wrap(err)
+	}
+	defer func() {
+		if err := vc.Close(); err != nil {
+			logger.Logger.Error("failed to close version client", err)
+		}
+	}()
+
+	vReq := &cxpb.CreateVersionRequest{
+		Parent: targetFlow(cfg),
+		Version: &cxpb.Version{
+			Name: version,
+		},
+	}
+	vOp, err := vc.CreateVersion(ctx, vReq)
+	if err != nil {
+		return failure.Wrap(fmt.Errorf("failed to create the version, error: %v", err))
+	}
+
+	_, err = vOp.Wait(ctx)
+	if err != nil {
+		return failure.Wrap(fmt.Errorf("failed to wait a create op, error: %v", err))
+	}
+
 	// update env for target
 
 	return nil
@@ -78,6 +106,10 @@ func targetAgent(cfg Config) string {
 
 func targetEnvironment(cfg Config) string {
 	return fmt.Sprintf("%s/locations/%s/agents/%s", targetAgent(cfg), cfg.TargetEnvID)
+}
+
+func targetFlow(cfg Config) string {
+	return fmt.Sprintf("%s/locations/%s/agents/%s", targetAgent(cfg), defaultFlowID)
 }
 
 func baseAgent(cfg Config) string {
